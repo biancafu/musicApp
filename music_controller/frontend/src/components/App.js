ec042f92f47fedfe97989110cdfc4974ebd28029
Bianca Fu <0509biancafu@gmail.com>
2023-08-21 00:29:52 -0600

    finish react project setup

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 //instead of import ReactDOM from "react-dom" then do ReactDom.render(_,_), we import {render} directly
-
+//but usually we just do this in index.js instead of like this which is creating a component App.js and import it to index.js
 
 export default class App extends Component {
     constructor(props) {
